96d4f2dc00e45a8d4895d85ede2d2b2e709513fa
Kendal Sosa <kendal.alfonso37@gmail.com>
2016-12-03 12:16:26 -0600

    Iniciado el proyecto proceso de markov con renovacion

---
+++ solucionCafeFamMarin.R
@@ -0,0 +1,13 @@
+# Proyecto de MEP115: Procesos de markov con Renovacion
+# Autores: 
+#
+# Gonzalez Ambrocio, Mario Abdiel   GA12015
+# Marin de la O, Bryan Humberto     MD13015
+# Sosa Montes, Kendal Alfonso       SM13001
+# 
+# Enunciado propuesto: Observar la ocurrencia de compra
+# de botes de cafe de la familia Marin y obtener los 
+# resultados por medio de una aplicacion estadistica 
+# para poder estudiar si este fenomeno cumple las 
+# condiciones de un proceso de markov con renovacion
+
